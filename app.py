3b1640bf0bd9280d29c14f423da04b36c175a5b3
wertemer <mpaderin@yahoo.com>
2021-05-22 01:22:35 +0300

    Add models db and migrations

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ template_path = os.path.join(project_root, './templates')
 app = Flask(__name__,template_folder=template_path)
 app.config.from_object(Configuration)
 db = SQLAlchemy(app)
-migrate = Migrate(app,db)
-db.create_all()
+
+migrate=Migrate(app,db) #Initializing migrate.
 
 #Нужно для использования уже существующие БД:
 Base = declarative_base()
@@ -25,3 +25,5 @@ Base = declarative_base()
 #Запросы
 engine = create_engine(Configuration.SQLALCHEMY_DATABASE_URI, convert_unicode=True, echo=False)
 Base.metadata.reflect(engine)
+#for migrations
+import models
--- models.py
+++ models.py
@@ -2,35 +2,38 @@ import re,datetime
 from app import db, Base
 
 class tUsers(db.Model):
+	__tablename__='tUsers'
 	id=db.Column(db.Integer,primary_key=True)
 	login=db.Column(db.String(120),nullable=False)
 	password=db.Column(db.String(12),nullable=False)
 
 class tAbout(db.Model):
+	__tablename__='tAbout'
 	id=db.Column(db.Integer,primary_key=True)
 	public=db.Column(db.DateTime,nullable=False)
 	about=db.Column(db.Text)
 
 class tPortfolio(db.Model):
+	__tablename__='tPortfolio'
 	id=db.Column(db.Integer,primary_key=True)
 	photo=db.Column(db.String(255),nullable=False)
 	desc=db.Column(db.String(255),nullable=True)
 
 class tContacts(db.Model):
-	__tablename__='contacts'
+	__tablename__='tContacts'
 	id=db.Column(db.Integer,primary_key=True)
 	contact=db.Column(db.String(255),nullable=True)
-	cico=db.relationship('tContactsIcons',backref='contacts',lazy='dynamic')
+	cico=db.relationship('tContactIcons',backref='tContacts',lazy='dynamic')
 
 class dIcons(db.Model):
-	__tablename__='icons'
+	__tablename__='dIcons'
 	id=db.Column(db.Integer,primary_key=True)
 	icon=db.Column(db.String(255),nullable=True)
 	desc=db.Column(db.String(255),nullable=True)
-	cico=db.relationship('tContactsIcons',backref='icons',lazy='dynamic')
+	cico=db.relationship('tContactIcons',backref='dIcons',lazy='dynamic')
 
 class tContactIcons(db.Model):
-	__tablename__='contacticonts'
+	__tablename__='tContactIcons'
 	id=db.Column(db.Integer,primary_key=True)
-	f_contact=db.Column(db.Integer,db.ForeignKey('contacts.id'))
-	f_icon=db.Column(db.Integer,db.ForeignKey('icons.id'))
+	f_contact=db.Column(db.Integer,db.ForeignKey('tContacts.id'))
+	f_icon=db.Column(db.Integer,db.ForeignKey('dIcons.id'))
